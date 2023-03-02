0231022c77110c351518fffc4a1add682eb671f6
menescots <agatamenes@gmail.com>
2023-03-02 14:53:41 +0100

    initial commit

---
+++ UberApp/UberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberAppApp.swift
+//  UberApp
+//
+//  Created by Agata Menes on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
